42b01b62092f5b13383d07c3f6493a2b7d582673
Peter Hedman <thephog@gmail.com>
2013-10-08 12:15:55 +0300

    Initial commit
    Implemented hello world for Windows Phone 8.

---
+++ StalkR/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using System.Windows.Threading;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using StalkR.Resources;
+    
+namespace StalkR
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        DispatcherTimer eventTimer;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            eventTimer          = new DispatcherTimer();
+            eventTimer.Interval = TimeSpan.FromSeconds(1.0);
+            eventTimer.Tick    += EventTimer_Tick;
+        }
+
+        void EventTimer_Tick(Object sender, EventArgs args)
+        {
+            eventTimer.Stop();
+            infoBox.Text = "";
+        }
+
+        private void IdentifyButton_Click(object sender, RoutedEventArgs e)
+        {
+            infoBox.Text = "Crunching numbers...";
+            eventTimer.Start();
+        }
+    }
+}
---
+++ StalkR/Properties/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Resources;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("StalkR")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("StalkR")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0a4d8620-a1fe-46ac-abe4-4984d523ef54")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("en-US")]
